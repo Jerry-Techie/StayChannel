65d213b67d03e9162866fbe44ad3848b55564181
Jerry <techjerry6@gmail.com>
2025-11-22 22:49:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // for Netlify
-  server: {
-    allowedHosts: [
-      'devserver-main--reliable-gnome-c05aac.netlify.app',
-      '.netlify.app'
-    ]
-  }
+  base: './',
 })
